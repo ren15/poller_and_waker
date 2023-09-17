3bb5dbb58afd10ba816beefb76da9a9cc69513e9
Ren Wang <iamwrm@gmail.com>
2023-09-17 09:58:50 +0800

    add version

--- src/a.py
+++ src/a.py
@@ -3,6 +3,7 @@ import time
 
 if __name__ == "__main__":
     print("a.py starting up")
+    version = 0
     con = sqlite3.connect("data/db.sqlite3")
     con.execute("DROP TABLE IF EXISTS deployment_meta")
     con.executescript(
@@ -12,22 +13,24 @@ if __name__ == "__main__":
                 id INTEGER PRIMARY KEY AUTOINCREMENT, 
                 name TEXT, 
                 status TEXT,
-                updated_at INTEGER
+                updated_at INTEGER,
+                version INTEGER
             );
         CREATE INDEX deployment_meta_name ON deployment_meta (name);
         CREATE INDEX deployment_meta_status ON deployment_meta (status);
         CREATE INDEX deployment_meta_updated_at ON deployment_meta (updated_at);
+        CREATE INDEX deployment_meta_version ON deployment_meta (version);
         """,
     )
     con.execute(
-        "INSERT INTO deployment_meta (name, status, updated_at) VALUES (?, ?, ?)",
-        ("a.py", "pending", time.time()),
+        "INSERT INTO deployment_meta (name, status, updated_at, version) VALUES (?, ?, ?, ?)",
+        ("a.py", "pending", time.time(), version),
     )
     time.sleep(5)
 
     con.execute(
-        "INSERT INTO deployment_meta (name, status, updated_at) VALUES (?, ?, ?)",
-        ("a.py", "ready", time.time()),
+        "INSERT INTO deployment_meta (name, status, updated_at, version) VALUES (?, ?, ?, ?)",
+        ("a.py", "success", time.time(), version),
     )
 
     con.commit()
